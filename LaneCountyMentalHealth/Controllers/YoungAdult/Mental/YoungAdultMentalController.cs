f740331d804d522264920fed6044a0624560649a
lemarrk80 <klemarr80@gmail.com>
2020-12-12 12:31:17 -0800

    added controllers for most of website

---
+++ LaneCountyMentalHealth/Controllers/YoungAdult/Mental/YoungAdultMentalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaneCountyMentalHealth.Controllers.YoungAdult.Mental
+{
+    public class YoungAdultMentalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LaneCountyMentalHealth/Controllers/Youth/Mental/YoungMentalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaneCountyMentalHealth.Controllers.Youth.Mental
+{
+    public class YoungMentalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
